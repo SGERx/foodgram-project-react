67bedf89e23b77651764585d6af2994309984e3c
sergey gerasimov <imbtoka@yandex.ru>
2023-06-03 13:51:09 +0300

    shopping cart2

---
+++ backend/foodgram/users/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+# from api.views import SubscriptionListView, SubscriptionView
+
+# urlpatterns = [
+#     path('users/subscriptions/',
+#          SubscriptionListView.as_view(),
+#          name='subscriptions'),
+#     path('users/<int:user_id>/subscribe/',
+#          SubscriptionView.as_view(),
+#          name='subscribe'),
+# ]
